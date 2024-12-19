775889cf0044b3e4a0798c3e0098dc47b09665f7
minecraft-gw <151071104+good-wars@users.noreply.github.com>
2024-12-19 10:04:31 +0300

    Обновление

--- app.py
+++ app.py
@@ -1,45 +1,72 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+from flask import request
+from markdown import markdown
+import requests
 import os
-import time
-import asyncio
-import htmlc as hc
 
 app = Flask(__name__)
 
-users = ['alex', 'john', 'bob']
+PASSWORD = os.environ.get('PASSWORD')
 
+@app.template_filter('markdown')
+def render_markdown(text):
+    return markdown(text)
+
+#  ДОМАШНЯЯ
+@app.route('/')
+@app.route('/<theme>/')
 @app.route('/<theme>')
-def home(theme = "light"):
-    if theme == 'light' or theme == 'dark':
-      return render_template('main/content/index.html', theme=theme)
-    else:
-      return render_template('main/content/index.html', theme='light')
+def home(theme="light"):
+    if theme in ['light', 'dark']:
+        return render_template('main/content/index.html', theme=theme)
+    return render_template('main/content/index.html', theme='light')
 
-
+#  НОВОСТИ
+@app.route('/<theme>/news/')
 @app.route('/<theme>/news')
-def news(theme = "light" and "dark"):
-    if theme == 'light' or theme == 'dark':
-      return render_template('news/content/index.html', theme=theme)
-    else:
-      return render_template('news/content/index.html', theme='light')
+def news(theme="light"):
+    if theme in ['light', 'dark']:
+        return render_template('news/content/index.html', theme=theme)
+    return render_template('news/content/index.html', theme='light')
 
+#  НОВОСТЬ
 @app.route('/<theme>/news.<news>')
-def news_content(news :str, theme = 'light'):
-  try:
-    return render_template(f'news/content/news/{news}.html', theme=theme, news=news)
-  except:
-    return render_template('news/error404.html')
+def news_content(news: str, theme='light'):
+    try:
+      fl = open(f'mdnews/{news}.md', 'r', encoding='utf-8')
+      fl = f"{fl.read()}"
+    except:
+      return render_template('/news/error404.html', theme=theme)
+    
+    url = "https://api.github.com/markdown"
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "X-GitHub-Api-Version": "2022-11-28",
+        "Authorization": "547570f885e89c45f6e50c16854f91303fb6fea6"
+    }
+    data = {
+        "text": fl,
+        "mode": "gfm"
+    }
 
-@app.route('/<theme>/profile/<username>')
-def show_user_profile(username):
-  global users
+    response = requests.post(url, json=data, headers=headers)
+    return render_template('markdown.html', content=response.text, theme=theme, news=news)
 
-  if username in users:
-    return f'User {username.title()}'
-  else:
-    return "Пользователь не найден!"
+@app.route("/<theme>/adpan", methods=['GET', 'POST'])
+def adpan(theme):
+    if request.method == 'POST':
+        password = request.form['password']
+        if password == PASSWORD:
+            return render_template("/adpan/adpan.html", theme=theme)
+    return render_template("/adpan/main.html", theme=theme)
+
+#  АККАУНТЫ FUNCOFF
+# @app.route('/<theme>/profile/<username>')
+# def show_user_profile(username):
+#     if username in users:
+#         return f'User {username.title()}'
+#     return "Пользователь не найден!"
+
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
--- start.py
+++ start.py
@@ -1,12 +1,18 @@
+import requests
 import os
-import time
-import asyncio
-import htmlc as hc
-import app
 
-def main():
-    app.app.run(debug=True)
+fl = open('mdnews/a.md', 'r', encoding='utf-8')
+fl = f"{fl.read()}"
 
-if __name__ == "__main__":
-    main()
-    asyncio.run(hc.start())
+url = "https://api.github.com/markdown"
+headers = {
+    "Accept": "application/vnd.github+json",
+    "X-GitHub-Api-Version": "2022-11-28",
+    "Authorization": "547570f885e89c45f6e50c16854f91303fb6fea6"
+}
+data = {
+    "text": fl,
+    "mode": "gfm"
+}
+
+response = requests.post(url, json=data, headers=headers)
