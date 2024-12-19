e6dd8cc8647dde1351aeb49313c3c9ba545d9e32
minecraft-gw <151071104+good-wars@users.noreply.github.com>
2024-12-19 10:17:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -69,4 +69,5 @@ def adpan(theme):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Используем PORT из переменной окружения или 5000 по умолчанию
+    app.run(host='0.0.0.0', port=port)
